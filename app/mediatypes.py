915562002c833be35bddaa65a64779dcb5a4499c
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-02-25 15:56:45 +0300

    Add common media types

--- app/mediatypes.py
+++ app/mediatypes.py
@@ -12,7 +12,26 @@ FOLDER = "application/directory"
 OCTET_STREAM = "application/octet-stream"
 
 mimetypes.init()
+
+mimetypes.add_type("application/sql", ".sql")
+mimetypes.add_type("application/x-zsh", ".zsh")
+mimetypes.add_type("text/jsx", ".jsx")
 mimetypes.add_type("text/markdown", ".md")
+mimetypes.add_type("text/plain", ".cfg")
+mimetypes.add_type("text/plain", ".ini")
+mimetypes.add_type("text/x-coffeescript", ".coffee")
+mimetypes.add_type("text/x-go", ".go")
+mimetypes.add_type("text/x-nim", ".nim")
+mimetypes.add_type("text/x-yml", ".yaml")
+mimetypes.add_type("text/x-yml", ".yml")
+mimetypes.add_type("text/x-python", ".pyi")
+mimetypes.add_type("text/x-python", ".pyx")
+mimetypes.add_type("text/x-swift", ".swift")
+mimetypes.add_type("text/x-plist", ".plist")
+mimetypes.add_type("text/x-rst", ".rst")
+mimetypes.add_type("text/x-rust", ".rs")
+mimetypes.add_type("text/x-toml", ".toml")
+mimetypes.add_type("text/x-vim", ".vim")
 
 
 def guess(name: StrOrPath, file: Optional[Union[bytes, IO[bytes]]] = None) -> str:
