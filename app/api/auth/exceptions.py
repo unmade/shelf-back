8ec5f7b969d353de642d54bddbd979205e2436d5
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-03-11 01:37:34 +0300

    Human readable API errors

--- app/api/auth/exceptions.py
+++ app/api/auth/exceptions.py
@@ -3,6 +3,6 @@ from app.api.exceptions import APIError
 
 class InvalidCredentials(APIError):
     status_code = 401
-    default_code = "INVALID_CREDENTIALS"
-    default_message = "Invalid credentials"
-    default_reason = "Incorrect email or password"
+    code = "INVALID_CREDENTIALS"
+    code_verbose = "Invalid credentials"
+    default_message = "Incorrect email or password"
--- app/api/files/exceptions.py
+++ app/api/files/exceptions.py
@@ -1,31 +1,98 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Optional
+
 from app.api.exceptions import APIError
 
-
-class AlreadyDeleted(APIError):
-    status_code = 400
-    default_code = "ALREADY_DELETED"
-    default_message = "Already deleted."
+if TYPE_CHECKING:
+    from app.typedefs import StrOrPath
 
 
-class AlreadyExists(APIError):
-    status_code = 400
-    default_code = "ALREADY_EXISTS"
-    default_message = "Already exists."
+class FilesError(APIError):
+    def __init__(self, message: Optional[str] = None, path: Optional[StrOrPath] = None):
+        super().__init__(message)
+        assert path is not None, "Missing required argument: 'path'"
+        self.message = self.get_message(path)
+
+    def get_message(self, path: StrOrPath) -> str:
+        raise NotImplementedError()
 
 
 class DownloadNotFound(APIError):
     status_code = 404
-    default_code = "DOWNLOAD_NOT_FOUND"
-    default_message = "Download not found."
+    code = "DOWNLOAD_NOT_FOUND"
+    code_verbose = "Download not found"
+    default_message = "Download is expired or doesn't exists"
 
 
-class InvalidPath(APIError):
+class FileAlreadyDeleted(FilesError):
     status_code = 400
-    default_code = "INVALID_PATH"
-    default_message = "Invalid path."
+    code = "ALREADY_DELETED"
+    code_verbose = "Already deleted"
+    default_message = "File '{name}' is already in the Trash"
+
+    def get_message(self, path: StrOrPath) -> str:
+        path = Path(path)
+        return self.message.format(name=path.name)
 
 
-class PathNotFound(APIError):
+class FileAlreadyExists(FilesError):
+    status_code = 400
+    code = "FILE_ALREADY_EXISTS"
+    code_verbose = "File already exists"
+    default_message = "The name '{name}' at path {parent} is already taken"
+
+    def get_message(self, path: StrOrPath) -> str:
+        path = Path(path)
+        return self.message.format(name=path.name, parent=path.parent)
+
+
+class IsADirectory(FilesError):
+    status_code = 400
+    code = "IS_A_DIRECTORY"
+    code_verbose = "Path is a directory"
+    default_message = "Expected the path '{path}' to be a file, but it is a folder"
+
+    def get_message(self, path: StrOrPath) -> str:
+        return self.message.format(path=path)
+
+
+class MalformedPath(APIError):
+    status_code = 400
+    code = "MALFORMED_PATH"
+    code_verbose = "Malformed path"
+    default_message = ""
+
+
+class NotADirectory(FilesError):
+    status_code = 400
+    code = "NOT_A_DIRECTORY"
+    code_verbose = "Not a directory"
+    default_message = (
+        "Expected the path '{path}' and its parents to be folders, not files"
+    )
+
+    def get_message(self, path: StrOrPath) -> str:
+        return self.message.format(path=path)
+
+
+class PathNotFound(FilesError):
     status_code = 404
-    default_code = "PATH_NOT_FOUND"
-    default_message = "Path not found."
+    code = "PATH_NOT_FOUND"
+    code_verbose = "Path not found"
+    default_message = "The path '{path}' does not exists"
+
+    def get_message(self, path: StrOrPath) -> str:
+        return self.message.format(path=path)
+
+
+class ThumbnailUnavailable(FilesError):
+    status_code = 400
+    code = "THUMBNAIL_UNAVAILABLE"
+    code_verbose = "Thumbnail unavailable"
+    default_message = "Can't generate thumbnail for a file '{name}' at path '{parent}'"
+
+    def get_message(self, path: StrOrPath) -> str:
+        path = Path(path)
+        return self.message.format(name=path.name, parent=path.parent)
