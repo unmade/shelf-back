3e531e893c1868a4bb548b7435dc449109116c52
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-04-07 00:30:37 +0300

    Set allowed origin with env

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 from datetime import timedelta
 from pathlib import Path
+from typing import Optional
 
 
 def _get_bool(key: str) -> bool:
@@ -12,9 +13,20 @@ def _get_bool(key: str) -> bool:
     return False
 
 
+def _get_list(key: str, default: Optional[list[str]] = None) -> list[str]:
+    value = os.getenv(key)
+    if value is not None:
+        return value.split(",")
+    else:
+        if default is not None:
+            return default
+    return []
+
+
 ACCESS_TOKEN_EXPIRE = timedelta(
     minutes=float(os.getenv("ACCESS_TOKEN_EXPIRE_IN_MINUTES", 15)),
 )
+
 
 APP_NAME = os.getenv("APP_NAME", "Shelf")
 APP_DEBUG = _get_bool("APP_DEBUG")
@@ -28,6 +40,8 @@ APP_SECRET_KEY = os.environ["APP_SECRET_KEY"]
 APP_VERSION = os.getenv("APP_VERSION")
 
 BASE_DIR = Path(__file__).absolute().resolve().parent.parent
+
+CORS_ALLOW_ORIGINS = _get_list("CORS_ALLOW_ORIGINS")
 
 EDGEDB_DSN = os.environ["EDGEDB_DSN"]
 
