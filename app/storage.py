3d6df7e884a2ac2eb42064b6e9ddc8beff2421a9
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-08-07 18:23:58 +0300

    Make `LocalStorage` async (#21)

--- app/storage.py
+++ app/storage.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import os
+import os.path
 import shutil
 from io import BytesIO
-from os.path import join
 from pathlib import Path
-from typing import IO, TYPE_CHECKING, Generator, Iterator, Type, TypeVar
+from typing import IO, TYPE_CHECKING, Generator, Iterator, TypeVar
 
 import zipfly
+from asgiref.sync import sync_to_async
 from PIL import Image, UnidentifiedImageError
 
 from app import config, errors
@@ -31,24 +32,12 @@ def _readchunks(path: StrOrPath) -> Generator[bytes, None, None]:
 class StorageFile:
     __slots__ = ("name", "path", "size", "mtime", "is_dir")
 
-    def __init__(self, name: str, path: Path, size: int, mtime: float, is_dir: bool):
+    def __init__(self, name: str, path: str, size: int, mtime: float, is_dir: bool):
         self.name = name
         self.path = path
         self.size = size
         self.mtime = mtime
         self.is_dir = is_dir
-
-    @classmethod
-    def from_path(cls: Type[T], path: StrOrPath, rel_to: StrOrPath) -> T:
-        path = Path(path)
-        stat = path.lstat()
-        return cls(
-            name=path.name,
-            path=path.relative_to(rel_to),
-            size=stat.st_size,
-            mtime=stat.st_mtime,
-            is_dir=path.is_dir(),
-        )
 
     def __str__(self) -> str:
         return str(self.path)
@@ -58,60 +47,37 @@ class LocalStorage:
     def __init__(self, root_dir: Path):
         self.root_dir = root_dir
 
-    def iterdir(self, path: StrOrPath) -> Iterator[StorageFile]:
-        dir_path = self.root_dir / path
+    def _from_path(self, path: StrOrPath) -> StorageFile:
+        path = Path(path)
+        stat = path.lstat()
+        return StorageFile(
+            name=path.name,
+            path=os.path.relpath(path, self.root_dir),
+            size=stat.st_size,
+            mtime=stat.st_mtime,
+            is_dir=path.is_dir(),
+        )
+
+    def _from_entry(self, entry: os.DirEntry[str]) -> StorageFile:
+        stat = entry.stat()
+        return StorageFile(
+            name=entry.name,
+            path=os.path.relpath(entry.path, self.root_dir),
+            size=stat.st_size,
+            mtime=stat.st_mtime,
+            is_dir=entry.is_dir(),
+        )
+
+    @sync_to_async
+    def delete(self, path: StrOrPath) -> None:
+        fullpath = os.path.join(self.root_dir, path)
         try:
-            return (
-                StorageFile.from_path(file, self.root_dir)
-                for file in dir_path.iterdir()
-            )
-        except NotADirectoryError as exc:
-            raise errors.NotADirectory() from exc
-
-    def save(self, path: StrOrPath, file: IO[bytes]) -> StorageFile:
-        file.seek(0)
-        fullpath = self.root_dir / path
-
-        try:
-            with fullpath.open("wb") as buffer:
-                shutil.copyfileobj(file, buffer)
-        except NotADirectoryError as exc:
-            raise errors.NotADirectory() from exc
-
-        return StorageFile.from_path(fullpath, self.root_dir)
-
-    def mkdir(self, path: StrOrPath) -> StorageFile:
-        dir_path = self.root_dir / path
-        try:
-            dir_path.mkdir(parents=True, exist_ok=True)
-        except FileExistsError as exc:
-            raise errors.FileAlreadyExists() from exc
-        except NotADirectoryError as exc:
-            raise errors.NotADirectory() from exc
-        return StorageFile.from_path(dir_path, self.root_dir)
-
-    def get(self, path: StrOrPath) -> StorageFile:
-        fullpath = self.root_dir / path
-        try:
-            return StorageFile.from_path(fullpath, self.root_dir)
+            if os.path.isdir(fullpath):
+                shutil.rmtree(fullpath)
+            else:
+                os.unlink(fullpath)
         except FileNotFoundError as exc:
             raise errors.FileNotFound() from exc
-
-    def is_exists(self, path: StrOrPath) -> bool:
-        fullpath = self.root_dir / path
-        return fullpath.exists()
-
-    def is_dir_exists(self, path: StrOrPath) -> bool:
-        fullpath = self.root_dir / path
-        return fullpath.exists() and fullpath.is_dir()
-
-    def move(self, from_path: StrOrPath, to_path: StrOrPath) -> None:
-        try:
-            shutil.move(self.root_dir / from_path, self.root_dir / to_path)
-        except FileNotFoundError as exc:
-            raise errors.FileNotFound() from exc
-        except NotADirectoryError as exc:
-            raise errors.NotADirectory() from exc
 
     def download(self, path: StrOrPath) -> Generator[bytes, None, None]:
         fullpath = self.root_dir / path
@@ -128,35 +94,61 @@ class LocalStorage:
 
         return _readchunks(fullpath)
 
-    def delete(self, path: StrOrPath) -> None:
+    @sync_to_async
+    def exists(self, path: StrOrPath) -> bool:
+        fullpath = os.path.join(self.root_dir, path)
+        return os.path.exists(fullpath)
+
+    @sync_to_async
+    def get(self, path: StrOrPath) -> StorageFile:
         fullpath = self.root_dir / path
         try:
-            if fullpath.is_dir():
-                shutil.rmtree(fullpath)
-            else:
-                fullpath.unlink()
+            return self._from_path(fullpath)
         except FileNotFoundError as exc:
             raise errors.FileNotFound() from exc
 
-    def delete_dir_content(self, path: StrOrPath) -> None:
+    @sync_to_async
+    def iterdir(self, path: StrOrPath) -> Iterator[StorageFile]:
+        dir_path = os.path.join(self.root_dir, path)
+        try:
+            return (self._from_entry(entry) for entry in os.scandir(dir_path))
+        except NotADirectoryError as exc:
+            raise errors.NotADirectory() from exc
+
+    @sync_to_async
+    def mkdir(self, path: StrOrPath) -> StorageFile:
+        dir_path = self.root_dir / path
+        try:
+            dir_path.mkdir(parents=True, exist_ok=True)
+        except FileExistsError as exc:
+            raise errors.FileAlreadyExists() from exc
+        except NotADirectoryError as exc:
+            raise errors.NotADirectory() from exc
+        return self._from_path(dir_path)
+
+    @sync_to_async
+    def move(self, from_path: StrOrPath, to_path: StrOrPath) -> None:
+        try:
+            shutil.move(self.root_dir / from_path, self.root_dir / to_path)
+        except FileNotFoundError as exc:
+            raise errors.FileNotFound() from exc
+        except NotADirectoryError as exc:
+            raise errors.NotADirectory() from exc
+
+    @sync_to_async
+    def save(self, path: StrOrPath, file: IO[bytes]) -> StorageFile:
+        file.seek(0)
         fullpath = self.root_dir / path
-        with os.scandir(fullpath) as it:
-            for entry in it:
-                if entry.is_dir():
-                    shutil.rmtree(entry.path)
-                else:
-                    os.unlink(entry.path)
 
-    def walk(
-        self, path: StrOrPath
-    ) -> Iterator[tuple[Path, Iterator[StorageFile], Iterator[StorageFile]]]:
-        for root, dirs, files in os.walk(self.root_dir / path):
-            yield (
-                Path(root).relative_to(self.root_dir),
-                (StorageFile.from_path(join(root, d), self.root_dir) for d in dirs),
-                (StorageFile.from_path(join(root, f), self.root_dir) for f in files),
-            )
+        try:
+            with fullpath.open("wb") as buffer:
+                shutil.copyfileobj(file, buffer)
+        except NotADirectoryError as exc:
+            raise errors.NotADirectory() from exc
 
+        return self._from_path(fullpath)
+
+    @sync_to_async
     def thumbnail(self, path: StrOrPath, size: int) -> tuple[int, IO[bytes]]:
         buffer = BytesIO()
         try:
@@ -174,5 +166,15 @@ class LocalStorage:
 
         return size, buffer
 
+    def walk(
+        self, path: StrOrPath
+    ) -> Iterator[tuple[Path, Iterator[StorageFile], Iterator[StorageFile]]]:
+        for root, dirs, files in os.walk(self.root_dir / path):
+            yield (
+                Path(root).relative_to(self.root_dir),
+                (self._from_path(os.path.join(root, d)) for d in dirs),
+                (self._from_path(os.path.join(root, f)) for f in files),
+            )
 
-storage = LocalStorage(config.STATIC_DIR)
+
+storage = LocalStorage(config.STORAGE_ROOT)
