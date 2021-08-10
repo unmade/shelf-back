a77bf01128c8bbc19ae3b1f730b489ec24af12ab
Aleksei Maslakov <lesha.maslakov@gmail.com>
2021-08-10 21:15:50 +0300

    Add base Storage class (#21)

--- app/typedefs.py
+++ app/typedefs.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from pathlib import Path
+    from pathlib import PurePath
     from uuid import UUID
     from edgedb import AsyncIOConnection, AsyncIOPool, AsyncIOTransaction
 
@@ -11,5 +11,5 @@ DBAnyConn = Union[AsyncIOConnection, AsyncIOPool, AsyncIOTransaction]
 DBConnOrPool = Union[AsyncIOConnection, AsyncIOPool]
 DBPool = AsyncIOPool
 
-StrOrPath = Union[str, Path]
+StrOrPath = Union[str, PurePath]
 StrOrUUID = Union[str, UUID]
