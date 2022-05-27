bc0599b7c89634cfb6f6e3f2003e92fa49db276f
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-05-27 19:07:46 +0300

    Restructure storage to multiple files (#73)

---
+++ app/storage/base.py
@@ -0,0 +1,227 @@
+from __future__ import annotations
+
+import abc
+from typing import IO, TYPE_CHECKING, Iterator
+
+if TYPE_CHECKING:
+    from app.typedefs import StrOrPath
+
+
+class StorageFile:
+    __slots__ = ("name", "ns_path", "path", "size", "mtime", "_is_dir")
+
+    def __init__(
+        self,
+        name: str,
+        ns_path: str,
+        path: str,
+        size: int,
+        mtime: float,
+        is_dir: bool
+    ):
+        self.name = name
+        self.ns_path = ns_path
+        self.path = path
+        self.size = size
+        self.mtime = mtime
+        self._is_dir = is_dir
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"name='{self.name}', "
+            f"ns_path='{self.ns_path}', "
+            f"path='{self.path}', "
+            f"size={self.size}, "
+            f"mtime={self.mtime}, "
+            f"is_dir={self.is_dir()}"
+            ")"
+        )
+
+    def __str__(self) -> str:
+        return f"{self.ns_path}:{self.path}"
+
+    def is_dir(self) -> bool:
+        """True if file is a directory, False otherwise."""
+        return self._is_dir
+
+
+class Storage:
+    def __init__(self, location: StrOrPath):
+        self.location = str(location)
+
+    @abc.abstractmethod
+    async def delete(self, ns_path: StrOrPath, path: StrOrPath) -> None:
+        """
+        Delete file or a folder by path.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+
+        Raises:
+            errors.FileNotFound: If file not found.
+        """
+
+    @abc.abstractmethod
+    def download(self, ns_path: StrOrPath, path: StrOrPath) -> Iterator[bytes]:
+        """
+        Return an iterator over a file content. If a file is a folder, then it will be
+        be a zip archive.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+
+        Yields:
+            Iterator[bytes]: Iterator to a file content.
+        """
+
+    @abc.abstractmethod
+    async def exists(self, ns_path: StrOrPath, path: StrOrPath) -> bool:
+        """
+        Check whether if file exists or not in the specified path.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+
+        Returns:
+            bool: True if file exists, False otherwise.
+        """
+
+    @abc.abstractmethod
+    async def get_modified_time(self, ns_path: StrOrPath, path: StrOrPath) -> float:
+        """
+        Get a datetime of the last modified time of the file.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+
+        Raises:
+            errors.FileNotFound: If file in path doesn't exists
+
+        Returns:
+            float: Last modified time of the file.
+        """
+
+    @abc.abstractmethod
+    async def iterdir(
+        self,
+        ns_path: StrOrPath,
+        path: StrOrPath,
+    ) -> Iterator[StorageFile]:
+        """
+        Return an iterator of StorageFile objects for a given path.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+
+        Raises:
+            errors.FileNotFound: If given path does not exist
+            errors.NotADirectory: If given path is not a directory
+
+        Yields:
+            Iterator[StorageFile]: Iterator of StorageFile objects.
+        """
+
+    @abc.abstractmethod
+    async def makedirs(self, ns_path: StrOrPath, path: StrOrPath) -> None:
+        """
+        Create a directory with any missing directories in a given path.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+
+        Raises:
+            errors.FileAlreadyExists: If some file already exists in a given path.
+            errors.NotADirectory: If some parent is not a directory.
+        """
+
+    @abc.abstractmethod
+    async def move(
+        self,
+        ns_path: StrOrPath,
+        from_path: StrOrPath,
+        to_path: StrOrPath,
+    ) -> None:
+        """
+        Move file or folder to a destination path. Destination path should include
+        source file name. For example to move file 'f.txt' to a folder 'b', destination
+        should as 'b/f.txt'.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            from_path (StrOrPath): Current file pathname relative to namespace.
+            to_path (StrOrPath): Next file pathname relative to namespace.
+
+        Raises:
+            errors.FileNotFound: If source or destination path does not exist.
+            errors.NotADirectory: If some parent of the destination is not a directory.
+        """
+
+    @abc.abstractmethod
+    async def save(
+        self,
+        ns_path: StrOrPath,
+        path: StrOrPath,
+        content: IO[bytes],
+    ) -> StorageFile:
+        """
+        Save content to a given path.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+            content (IO[bytes]): Content to save.
+
+        Raises:
+            errors.NotADirectory: If some parent is not a directory.
+
+        Returns:
+            StorageFile: Saved StorageFile.
+        """
+
+    @abc.abstractmethod
+    async def size(self, ns_path: StrOrPath, path: StrOrPath) -> int:
+        """
+        Get the total size, in bytes, of the file referenced by path.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+
+        Raises:
+            errors.FileNotFound: If given path does not exist.
+
+        Returns:
+            int: Total size in bytes.
+        """
+
+    @abc.abstractmethod
+    async def thumbnail(
+        self,
+        ns_path: StrOrPath,
+        path: StrOrPath,
+        size: int,
+    ) -> tuple[int, IO[bytes]]:
+        """
+        Generate thumbnail with a specified size for the given path.
+
+        Args:
+            ns_path (StrOrPath): Namespace path.
+            path (StrOrPath): File pathname relative to namespace.
+            size (int): Size of thumbnail in pixels.
+
+        Raises:
+            errors.FileNotFound: If given path does not exist.
+            errors.IsADirectory: If given path is a directory.
+            errors.ThumbnailUnavailable: If image type is not supported or file
+                is not an image.
+
+        Returns:
+            tuple[int, IO[bytes]]: Size, in bytes and a thumbnail.
+        """
---
+++ tests/test_storage/test_filesystem.py
@@ -0,0 +1,316 @@
+from __future__ import annotations
+
+import operator
+import shutil
+from io import BytesIO
+from typing import IO, TYPE_CHECKING
+from zipfile import ZipFile
+
+import pytest
+from asgiref.sync import sync_to_async
+
+from app import errors
+from app.storage.filesystem import FileSystemStorage
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from app.typedefs import StrOrPath
+
+pytestmark = [pytest.mark.asyncio]
+
+
+@pytest.fixture
+def file_factory(tmp_path: Path):
+    """
+    A file factory for a FileSystemStorage.
+
+    Save file in a specified path with a given content and return full path.
+    Any missing parents will be created.
+    """
+    @sync_to_async
+    def create_file(
+        path: StrOrPath,
+        content: IO[bytes] = BytesIO(b"I'm Dummy File!"),
+    ) -> Path:
+        fullpath = tmp_path / path
+        fullpath.parent.mkdir(parents=True, exist_ok=True)
+        with open(fullpath, 'wb') as buffer:
+            shutil.copyfileobj(content, buffer)
+
+        return fullpath
+
+    return create_file
+
+
+@pytest.fixture
+def fs_storage(tmp_path: Path) -> FileSystemStorage:
+    """An instance of FileSystemStorage with `tmp_path` fixture as a location."""
+    return FileSystemStorage(tmp_path)
+
+
+async def test_delete_file(file_factory, fs_storage: FileSystemStorage):
+    fullpath = await file_factory("user/f.txt")
+    assert fullpath.exists()
+    await fs_storage.delete("user", "f.txt")
+    assert not fullpath.exists()
+
+
+async def test_delete_folder(file_factory, fs_storage: FileSystemStorage):
+    fullpath = await file_factory("user/a/f.txt")
+    assert fullpath.exists()
+    await fs_storage.delete("user", "a")
+    assert not fullpath.exists()
+    assert not fullpath.parent.exists()
+
+
+async def test_delete_but_file_does_not_exist(fs_storage: FileSystemStorage):
+    with pytest.raises(errors.FileNotFound):
+        await fs_storage.delete("user", "f.txt")
+
+
+async def test_download_file(file_factory, fs_storage: FileSystemStorage):
+    await file_factory("user/f.txt")
+    buffer = BytesIO()
+    for chunk in fs_storage.download("user", "f.txt"):
+        buffer.write(chunk)
+    buffer.seek(0)
+    assert buffer.read() == b"I'm Dummy File!"
+
+
+async def test_download_folder(file_factory, fs_storage: FileSystemStorage):
+    await file_factory("user/a/x.txt", content=BytesIO(b"Hello"))
+    await file_factory("user/a/y.txt", content=BytesIO(b"World"))
+
+    await file_factory("user/a/b/z.txt", content=BytesIO(b"!"))
+    buffer = BytesIO()
+    for chunk in fs_storage.download("user", "a"):
+        buffer.write(chunk)
+    buffer.seek(0)
+
+    with ZipFile(buffer, "r") as archive:
+        assert sorted(archive.namelist()) == sorted(["x.txt", "y.txt", "b/z.txt"])
+        assert archive.read("x.txt") == b"Hello"
+        assert archive.read("y.txt") == b"World"
+        assert archive.read("b/z.txt") == b"!"
+
+
+async def test_exists(file_factory, fs_storage: FileSystemStorage):
+    assert not await fs_storage.exists("user", "a")
+    await file_factory("user/a/f.txt")
+    assert await fs_storage.exists("user", "a")
+    assert await fs_storage.exists("user", "a/f.txt")
+
+
+async def test_get_modified_time(file_factory, fs_storage: FileSystemStorage):
+    fullpath = await file_factory("user/a/f.txt")
+    mtime = await fs_storage.get_modified_time("user", "a/f.txt")
+    assert mtime == fullpath.lstat().st_mtime
+
+    mtime = await fs_storage.get_modified_time("user", "a")
+    assert mtime == fullpath.parent.lstat().st_mtime
+
+
+async def test_get_modified_time_but_file_does_not_exist(fs_storage: FileSystemStorage):
+    with pytest.raises(errors.FileNotFound):
+        await fs_storage.get_modified_time("user", "f.txt")
+
+
+async def test_iterdir(file_factory, fs_storage: FileSystemStorage):
+    await file_factory("user/a/x.txt")
+    await file_factory("user/a/y.txt")
+    await file_factory("user/b/z.txt")
+
+    x, y = sorted(
+        await fs_storage.iterdir("user", "a"),
+        key=operator.attrgetter("path")
+    )
+
+    assert x.name == "x.txt"
+    assert x.ns_path == "user"
+    assert x.path == "a/x.txt"
+    assert y.name == "y.txt"
+    assert y.ns_path == "user"
+    assert y.path == "a/y.txt"
+
+
+async def test_iterdir_does_not_include_broken_symlinks(
+    tmp_path: Path,
+    file_factory,
+    fs_storage: FileSystemStorage,
+):
+    await file_factory("user/x.txt")
+    fullpath = await file_factory("user/y.txt")
+    (tmp_path / "user/y_symlink.txt").symlink_to(fullpath)
+    fullpath.unlink()
+
+    files = list(await fs_storage.iterdir("user", "."))
+    assert(len(files)) == 1
+    assert files[0].path == "x.txt"
+
+
+async def test_iterdir_but_path_does_not_exist(fs_storage: FileSystemStorage):
+    with pytest.raises(errors.FileNotFound):
+        list(await fs_storage.iterdir("user", "a"))
+
+
+async def test_iterdir_but_it_is_a_file(file_factory, fs_storage: FileSystemStorage):
+    await file_factory("user/f.txt")
+    with pytest.raises(errors.NotADirectory):
+        list(await fs_storage.iterdir("user", "f.txt"))
+
+
+async def test_makedirs(tmp_path: Path, fs_storage: FileSystemStorage):
+    assert not (tmp_path / "user/a").exists()
+
+    await fs_storage.makedirs("user", "a")
+    assert (tmp_path / "user/a").exists()
+
+    await fs_storage.makedirs("user", "a/b/c")
+    assert (tmp_path / "user/a/b/c").exists()
+
+
+async def test_makedirs_but_file_already_exists(
+    file_factory, fs_storage: FileSystemStorage
+):
+    await file_factory("user/a")
+
+    with pytest.raises(errors.FileAlreadyExists):
+        await fs_storage.makedirs("user", "a")
+
+
+async def test_makedirs_but_parent_is_a_directory(
+    file_factory, fs_storage: FileSystemStorage
+):
+    await file_factory("user/x.txt")
+
+    with pytest.raises(errors.NotADirectory):
+        await fs_storage.makedirs("user", "x.txt/y.txt")
+
+
+async def test_move_file(
+    tmp_path: Path,
+    file_factory,
+    fs_storage: FileSystemStorage,
+):
+    await file_factory("user/x.txt")
+
+    await fs_storage.move("user", "x.txt", "y.txt")
+
+    assert not (tmp_path / "user/x.txt").exists()
+    assert (tmp_path / "user/y.txt").exists
+
+
+async def test_move_folder(
+    tmp_path: Path,
+    file_factory,
+    fs_storage: FileSystemStorage,
+):
+    await file_factory("user/a/f.txt")
+    await file_factory("user/b/f.txt")
+
+    # move folder 'a' to 'a/b' under name 'a'
+    await fs_storage.move("user", "a", "b/a")
+
+    assert not (tmp_path / "user/a").exists()
+    assert (tmp_path / "user/b/a/f.txt").exists()
+
+
+async def test_move_but_source_does_not_exist(fs_storage: FileSystemStorage):
+    with pytest.raises(errors.FileNotFound):
+        await fs_storage.move("user", "x.txt", "y.txt")
+
+
+async def test_move_but_destination_does_not_exist(
+    file_factory, fs_storage: FileSystemStorage
+):
+    await file_factory("user/x.txt")
+    with pytest.raises(errors.FileNotFound):
+        await fs_storage.move("user", "x.txt", "a/y.txt")
+
+
+async def test_move_but_destination_is_not_a_directory(
+    file_factory, fs_storage: FileSystemStorage
+):
+    await file_factory("user/x.txt")
+    await file_factory("user/y.txt")
+    with pytest.raises(errors.NotADirectory):
+        await fs_storage.move("user", "x.txt", "y.txt/x.txt")
+
+
+async def test_save(tmp_path: Path, fs_storage: FileSystemStorage):
+    await fs_storage.makedirs("user", "a")
+    content = BytesIO(b"I'm Dummy file!")
+    file = await fs_storage.save("user", "a/f.txt", content=content)
+
+    assert (tmp_path / "user/a/f.txt").exists()
+    assert file.name == "f.txt"
+    assert file.ns_path == "user"
+    assert file.path == "a/f.txt"
+    assert file.size == 15
+    assert file.is_dir() is False
+
+
+async def test_save_but_path_is_not_a_folder(
+    file_factory, fs_storage: FileSystemStorage
+):
+    await file_factory("user/f.txt")
+
+    with pytest.raises(errors.NotADirectory):
+        await fs_storage.save("user", "f.txt/f.txt", content=BytesIO(b""))
+
+
+async def test_save_overrides_existing_file(
+    file_factory, fs_storage: FileSystemStorage
+):
+    fullpath = await file_factory("user/f.txt")
+    assert fullpath.lstat().st_size == 15
+    file = await fs_storage.save("user", "f.txt", content=BytesIO(b""))
+    assert file.size == 0
+
+
+async def test_size(file_factory, fs_storage: FileSystemStorage):
+    fullpath = await file_factory("user/f.txt")
+    size = await fs_storage.size("user", "f.txt")
+    assert size == fullpath.lstat().st_size
+
+
+async def test_size_but_file_does_not_exist(fs_storage: FileSystemStorage):
+    with pytest.raises(errors.FileNotFound):
+        await fs_storage.size("user", "f.txt")
+
+
+async def test_thumbnail(
+    file_factory, image_content, fs_storage: FileSystemStorage
+):
+    await file_factory("user/im.jpg", content=image_content)
+    size, content = await fs_storage.thumbnail("user", "im.jpg", size=128)
+    assert size == 883
+    assert size == len(content.read())
+
+
+async def test_thumbnail_but_path_is_a_directory(
+    file_factory, image_content, fs_storage: FileSystemStorage
+):
+    await file_factory("user/a/im.jpg", content=image_content)
+
+    with pytest.raises(errors.IsADirectory) as excinfo:
+        await fs_storage.thumbnail("user", "a", size=128)
+
+    assert str(excinfo.value) == "Path 'a' is a directory"
+
+
+async def test_thumbnail_but_file_is_not_an_image(
+    file_factory, fs_storage: FileSystemStorage
+):
+    await file_factory("user/im.jpg")
+
+    with pytest.raises(errors.ThumbnailUnavailable) as excinfo:
+        await fs_storage.thumbnail("user", "im.jpg", size=128)
+
+    assert str(excinfo.value) == "Can't generate thumbnail for a file: 'im.jpg'"
+
+
+async def test_thumbnail_but_path_does_not_exist(fs_storage: FileSystemStorage):
+    with pytest.raises(errors.FileNotFound):
+        await fs_storage.thumbnail("user", "im.jpg", size=128)
