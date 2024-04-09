17371294b4a92d8a56facef3db4c80faeb2cd265
Aleksei Maslakov <lesha.maslakov@gmail.com>
2024-04-09 23:53:00 +0300

    Add dummy endpoint to list albums

--- app/api/photos/__init__.py
+++ app/api/photos/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .albums.views import router as albums_router
 from .media_items.internal import router as media_items_internal_router
 from .media_items.views import router as media_items_router
 
@@ -10,6 +11,7 @@ __all__ = [
 
 router = APIRouter()
 
+router.include_router(albums_router, prefix="/albums", tags=["albums"])
 router.include_router(media_items_router, prefix="/media_items", tags=["media_items"])
 router.include_router(
     media_items_internal_router, prefix="/-/media_items", include_in_schema=False
